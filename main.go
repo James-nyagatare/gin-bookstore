fd9715f918bf1660617287198f7805ca035ba3c8
James <james.nyagatare@codeland.it>
2021-08-11 11:47:33 +0200

    feat(routes): Add routes
    - Adds crud routes and controllers

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/James-nyagatare/gin-bookstore/models"
+	"github.com/James-nyagatare/gin-bookstore/controllers"
 	"github.com/gin-gonic/gin"
 )
 
@@ -9,6 +10,12 @@ func main() {
 	r := gin.Default()
 
 	models.ConnectDatabase()
+
+	r.GET("/books", controllers.FindBooks)
+	r.POST("/books", controllers.CreateBook)
+	r.GET("/books/:id", controllers.FindBook)
+	r.PATCH("/books/:id", controllers.UpdateBook)
+	r.DELETE("/books/:id", controllers.DeleteBook)
 
 	r.Run()
 }
